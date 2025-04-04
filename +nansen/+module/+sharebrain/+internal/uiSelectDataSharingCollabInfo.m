59054f4a6bbb3979f2ea335492f924075508cbad
ehennestad <ehennestad@gmail.com>
2025-04-04 17:23:28 +0200

    Update session method for connecting to ebrains dataset

---
+++ +nansen/+module/+sharebrain/+internal/uiSelectDataSharingCollabInfo.m
@@ -0,0 +1,31 @@
+function collabInfo = uiSelectDataSharingCollabInfo(options)
+    arguments
+        options.TargetFolder (1,:) string {mustBeFolder}
+        %options.Logger % Todo
+    end
+    
+    myClient = ebrains.collaboratory.api.Collab();
+    fprintf('Please wait, fetching datasets...\n')
+    [~,result,~] = myClient.searchCollab('search', 'Dataset working title', 'limit', 1000, 'visibility', 'private');
+    
+    [~, isSelection] = sort([result.createDate], 'descend');
+    collab_info = result(isSelection);
+
+    % parse description:
+    expression = '(?<=Dataset working title: ).+?(?=\n)';
+    datasetTitles = regexp([collab_info.description], expression, 'match', 'once')';
+
+    selection = uim.dialog.searchSelectDlg(cellstr(datasetTitles), 'Select a Dataset');
+    
+    if isempty(selection)
+        error('Aborted')
+    end
+
+    isSelection = strcmp(datasetTitles, selection);
+    collabInfo = collab_info(isSelection);
+
+    if isfield(options, "TargetFolder") && ~isempty(options.TargetFolder)
+        filePath = fullfile(options.TargetFolder, 'ebrains_collab_info.json');
+        utility.filewrite(filePath, jsonencode(collabInfo))
+    end
+end
--- +nansen/+module/+sharebrain/+mixin/+tool/+sharebrain/setDataset.m
+++ +nansen/+module/+sharebrain/+mixin/+tool/+sharebrain/setDataset.m
@@ -1,3 +1,18 @@
 function setDataset()
-disp('a')
+    project = nansen.getCurrentProject();
+    configFolder = project.getConfigurationFolder();
+    
+    filePath = fullfile(configFolder, 'ebrains_collab_info.json');
+
+    if isfile(filePath)
+        answer = questdlg('EBRAINS Dataset is already configured. Do you want to change this dataset?', 'Reset dataset?', 'Yes', 'No', 'Cancel', 'No');
+        if strcmp(answer, 'Yes')
+            %pass
+        else
+            return
+        end
+    end
+
+    nansen.module.sharebrain.internal.uiSelectDataSharingCollabInfo(...
+        "TargetFolder", configFolder)
 end
