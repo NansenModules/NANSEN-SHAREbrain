46365f1deaa84060f90b20da603eff7ce41b451e
ehennestad <ehennestad@gmail.com>
2025-10-15 20:39:18 +0200

    Update initializeFileRepo.m
    Use omkg sync toolbox to initialize a file repo

--- +nansen/+module/+sharebrain/+mixin/+tool/+sharebrain/+metadata/initializeFileRepo.m
+++ +nansen/+module/+sharebrain/+mixin/+tool/+sharebrain/+metadata/initializeFileRepo.m
@@ -8,34 +8,20 @@ function initializeFileRepo()
     
     collabId = nansen.module.sharebrain.internal.getCollabIdFromProject(project);
     space = sprintf('collab-%s', collabId);
-    
-    kgcol = KgCollection('Space', space);
 
     dsvUuid = collabId(3:end);
-    tic
-    fgdsv = kgcol.getInstance(dsvUuid);
-    toc
-    tic
-    dsv = kgcol.getOpenMindsInstance(dsvUuid);
-    toc
+    dsv = kgpull(dsvUuid);
 
-    %dsv = kgcol.getFromId(dsvUuid, 'openminds.core.products.DatasetVersion');
-
-
-    if isa(dsv.repository, 'py.NoneType')
-
-        repo = kgcol.list(class(openminds.core.FileRepository));
-           
-        if isempty( repo )
-            repo = openminds.core.FileRepository();
-            repo.name = sprintf('bucket/%s', collabId);
-            repo.IRI = sprintf("https://data-proxy.ebrains.eu/api/v1/buckets/%s", collabId);
-            repo.type = "Swift";
-            dsv.repository = kgcol.convertOpenMINDSInstance(repo);
-        else
-            dsv.repository = repo;
-        end
-
-        kgcol.updateInstance(dsv)
+    if isempty(dsv.repository)
+        repo = openminds.core.FileRepository();
+        repo.name = sprintf('bucket/%s', collabId);
+        repo.IRI = sprintf("https://data-proxy.ebrains.eu/api/v1/buckets/%s", collabId);
+        repo.type = "Swift";
+        dsv.repository = repo;
+        
+        kgsave(dsv, "space", space)
+    else
+        disp('This Dataset version already has a repository.')
+        %repoRef = dsv.repository.resolve();
     end
 end
